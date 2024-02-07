aeb37241d0ea6f3ded6e18a62e442b8330e5e1dd
Ubuntu <ubuntu@ip-172-31-6-147.eu-west-1.compute.internal>
2024-02-07 22:15:32 +0000

    import iata in elastic db

---
+++ tests/db_iata_import.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+import json
+
+# Connexion à Elasticsearch
+es = Elasticsearch(['http://localhost:9200'])  # Remplacez localhost:9200 par l'adresse de votre cluster Elasticsearch
+
+# Nom de l'index Elasticsearch
+index_name = "iata"
+
+# Ouvrir le fichier JSON
+with open('iata.json', 'r') as file:
+    # Charger chaque ligne du fichier JSON séparément
+    for line in file:
+        # Charger le document JSON de la ligne
+        doc = json.loads(line)
+        
+        # Indexer le document dans Elasticsearch
+        es.index(index=index_name, body=doc)
+
+# Rafraîchir l'index pour rendre les documents disponibles pour la recherche
+es.indices.refresh(index=index_name)
+
+print("Import JSON to Elasticsearch completed successfully.")
